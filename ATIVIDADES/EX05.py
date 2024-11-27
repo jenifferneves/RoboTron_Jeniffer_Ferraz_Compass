8286b6e416002379a53cfaa92c8e39cf5e8ee33c
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-11-27 13:23:39 -0300

    Exercício 5: somar a média aritmética dos números pares.

---
+++ ATIVIDADES/EX05.py
@@ -0,0 +1,13 @@
+valores_pares = []  
+
+print("Digite 20 valores para X:")
+for i in range(1, 21):
+    valor = int(input(f"Digite o valor {i}: "))
+    if valor % 2 == 0:  
+        valores_pares.append(valor)
+
+if valores_pares:
+    media_pares = sum(valores_pares) / len(valores_pares)
+    print(f"\nA média aritmética dos valores pares é: {media_pares:.2f}")
+else:
+    print("\nNenhum valor par foi digitado.")
