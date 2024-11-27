5cba0a8ae2089c0545844c7db919d1c14e2c87c6
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-11-27 13:31:47 -0300

    Exercício 3: soma de dois valores e saída do resultado se é impar ou par.

--- ATIVIDADES/EX03.py
+++ ATIVIDADES/EX03.py
@@ -1,5 +1,5 @@
-valor1 = int(input("Digite o primeiro valor: "))
-valor2 = int(input("Digite o segundo valor: "))
+valor1 = int(input("Digite o Primeiro valor: "))
+valor2 = int(input("Digite o Segundo valor: "))
 
 soma = valor1 + valor2
 
