be18157d069c913137071f7c5f8fa8d4878e6bcc
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-11-28 09:44:40 -0300

    Exercício 8: Calcular o fatorial de X e mostrar a tabuada caso a entrada for impar.

---
+++ ATIVIDADES/EX08.py
@@ -0,0 +1,11 @@
+valor = int(input("Digite um valor inteiro para X: "))
+
+if valor % 2 == 0:
+    fatorial = 1
+    for i in range(1, valor + 1):
+        fatorial *= i
+    print(f"O fatorial de {valor} é {fatorial}")
+else:
+    print(f"Tabuada de {valor}:")
+    for i in range(1, 11):
+        print(f"{valor} x {i} = {valor * i}")
