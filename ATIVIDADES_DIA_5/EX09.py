3f8a7a398fa244b0387eba409f44d5f80c2cca4b
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-02 13:16:17 -0300

    Exercício 9: Entrada de 15 valores e saída deles ao contrario.

--- ATIVIDADES_DIA_5/EX09.py
+++ ATIVIDADES_DIA_5/EX09.py
@@ -1,7 +1,7 @@
 valores = []
 
 for i in range(1, 16):
-    valor = input(f"Digite o valor {i}: ")
+    valor = input(f"Digite o Valor {i}: ")
     valores.append(valor)
 
 print("\nValores da lista do último para o primeiro:")
