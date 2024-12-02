6abad8bccaaecb389bae2403d648455e1a36162d
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-02 13:12:22 -0300

    Exercício 6: Entrada maior que 2 e a saída de valores impares.

--- ATIVIDADES_DIA_5/EX06.py
+++ ATIVIDADES_DIA_5/EX06.py
@@ -1,7 +1,7 @@
 x = int(input("Digite um valor inteiro maior que 2: "))
 
 if x > 2:
-    print(f"Os Números ímpares entre 0 e {x} são:")
+    print(f"Os números ímpares entre 0 e {x} são:")
     for i in range(x + 1):
         if i % 2 != 0:
             print(i, end=" ")
