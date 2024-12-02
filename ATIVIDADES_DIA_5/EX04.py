dac9079f2383d4178abb160716aebca010b8c27d
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-02 13:08:49 -0300

    Exercício 4: programa que compara se a idade declarada é maior de idade, menor de idade ou criança.

--- ATIVIDADES_DIA_5/EX04.py
+++ ATIVIDADES_DIA_5/EX04.py
@@ -1,8 +1,8 @@
 idade = int(input("Digite a sua idade: "))
 
 if idade > 18:
-    print("Maior de Idade")
+    print("Maior de idade")
 elif idade < 12:
-    print("Você é uma Criança")
+    print("Você é uma criança")
 else:
-    print("Você é um Adolescente")
+    print("Você é um adolescente")
