53538a87487dec0f6d0fcbfdd46f757476c7e187
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-02 12:59:55 -0300

    REVISÃO

---
+++ ATIVIDADE_DIA_7/EX01.py
@@ -0,0 +1,14 @@
+import json
+
+def retornar_json():
+   with open(r"C:\Users\jeniffer.neves\source\repos\jenifferneves\RoboTron_Jeniffer_Ferraz_Compass\ATIVIDADE_DIA_7\partida.json", encoding="utf-8") as json_normal:
+    json_manipulavel = json.load(json_normal)
+    return json_manipulavel
+
+
+json_retornado = retornar_json()
+
+print(json_retornado)
+
+
+
