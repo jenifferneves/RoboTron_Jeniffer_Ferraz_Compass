b84d14ee393598531186817703072b73b0d2448c
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-04 13:11:01 -0300

    Exercício 3- Printar Nome do Estádio, o Placar e o Status do jogo.

--- ATIVIDADE_DIA_7/EX03.py
+++ ATIVIDADE_DIA_7/EX03.py
@@ -1,23 +1,20 @@
 import json
 
 def retornar_json():
-    # Abrindo o arquivo JSON
+    
     with open(r"C:\Users\jeniffer.neves\source\repos\jenifferneves\RoboTron_Jeniffer_Ferraz_Compass\ATIVIDADE_DIA_7\partida.json", encoding="utf-8") as json_normal:
         json_manipulavel = json.load(json_normal)
         return json_manipulavel
 
-# Carregar o JSON
 json_retornado = retornar_json()
 
-# Extrair as informações desejadas
-partida = json_retornado["copa-do-brasil"][0]  # Pegando a primeira partida
+partida = json_retornado["copa-do-brasil"][0] 
 nome_estadio = partida["estadio"]["nome_popular"]
 placar = partida["placar"]
 status = partida["status"]
 
-# Mostrar os dados
 print(json.dumps({
-    "Nome do Estadio": nome_estadio,  # Corrigido aqui
+    "Nome do Estadio": nome_estadio,
     "Placar": placar,
     "Status": status
 }, indent=4, ensure_ascii=False))
