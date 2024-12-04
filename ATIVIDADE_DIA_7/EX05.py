561ad19cdc438e1481f1b437c40a83a0a09e9b9b
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-04 12:45:28 -0300

    Exercício 5- nomeando como campeonato"JSON 2" em uma variável e printe printando todos os dados.

---
+++ ATIVIDADE_DIA_7/EX05.py
@@ -0,0 +1,11 @@
+import json
+
+def retornar_json():
+   
+    with open(r"C:\Users\jeniffer.neves\source\repos\jenifferneves\RoboTron_Jeniffer_Ferraz_Compass\ATIVIDADE_DIA_7\campeonato.json", encoding="utf-8") as json_normal:
+        json_manipulavel = json.load(json_normal)
+        return json_manipulavel
+
+json_retornado = retornar_json()
+
+print(json.dumps(json_retornado, indent=4, ensure_ascii=False))
