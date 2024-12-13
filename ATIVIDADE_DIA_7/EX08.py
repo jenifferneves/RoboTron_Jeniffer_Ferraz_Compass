0e314d3a9b23ae6f4f023d69f1f7f3eac464cb82
Jeniffer Ferraz das Neves <jeniffer.neves@compasso.com.br>
2024-12-13 13:33:34 -0300

    Atualização

--- ATIVIDADE_DIA_7/EX08.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-arquivo = pd.read_csv(r"C:\Users\jeniffer.neves\source\repos\jenifferneves\RoboTron_Jeniffer_Ferraz_Compass\ATIVIDADE_DIA_7\pandas.csv", encoding="latin-1", sep=",")
-
-pd.set_option('display.max_rows', None)
-
-print(arquivo)
